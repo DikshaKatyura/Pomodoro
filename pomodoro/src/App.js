b463be6548ac34b15656746fcae260d5ce00e3ff
loadingthedev <loadingthedev@gmail.com>
2023-08-07 17:41:21 +0530

    removed app.css

--- pomodoro/src/App.js
+++ pomodoro/src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import SectionOne from "./components/sections/SectionOne";
 import SectionTwo from "./components/sections/SectionTwo";
 import TimerContextProvider from "./contextAPIs/TimerContextProvider";
